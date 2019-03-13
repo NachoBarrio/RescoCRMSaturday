07b63c58aaa0438b372d5c6cbce4f4a9d7a16b9f
ignacio.barrio <ignacio.barrio@codere.com>
2019-03-13 20:54:58 +0100

    Prototipo de proyecto para recursos web

---
+++ RescoD365Saturday/RescoD365Saturday/js/BookSessions.js
@@ -0,0 +1,13 @@
+﻿/***
+    Library to book mysessions for D365 Saturday
+    A contact will book his sessions during the event
+
+**/
+var mySessions = function () {
+
+
+
+    return {
+
+    }
+}();
